d14d756e6cda8df691fbbb1a870d44c73a40db5d
joao96 <jvpoletti@gmail.com>
2016-09-23 22:52:29 -0300

    Teste para as dezenas

--- Gteste.c
+++ Gteste.c
@@ -2,6 +2,23 @@
 #include "funcao.h"
 #include <gtest/gtest.h>
 
+/**
+ \brief Função da biblioteca "gtest.h" que será usada como ferramenta para automatizar os testes das UNIDADES.
+*/
+
+TEST(retorna_n_arabico, UNIDADES)
+{
+	EXPECT_EQ(1,retorna_n_arabico("I")); // Dada a entrada "I", espera-se obter o retorno de 1.
+	EXPECT_EQ(2,retorna_n_arabico("II")); // Dada a entrada "II", espera-se obter o retorno de 2.
+	EXPECT_EQ(3,retorna_n_arabico("III")); // Dada a entrada "III", espera-se obter o retorno de 3.
+	EXPECT_EQ(4,retorna_n_arabico("IV")); // Dada a entrada "IV", espera-se obter o retorno de 4.
+	EXPECT_EQ(5,retorna_n_arabico("V")); // Dada a entrada "V", espera-se obter o retorno de 5.
+	EXPECT_EQ(6,retorna_n_arabico("VI")); // Dada a entrada "VI", espera-se obter o retorno de 6.
+	EXPECT_EQ(7,retorna_n_arabico("VII")); // Dada a entrada "VII", espera-se obter o retorno de 7.
+	EXPECT_EQ(8,retorna_n_arabico("VIII")); // Dada a entrada "VIII", espera-se obter o retorno de 8.
+	EXPECT_EQ(9,retorna_n_arabico("IX")); // Dada a entrada "IX", espera-se obter o retorno de 9.
+	EXPECT_EQ(10,retorna_n_arabico("X")); // Dada a entrada "X", espera-se obter o retorno de 10.
+}
 
 TEST(checa_existencia_romanTESTE, Invalidez)
 {
--- funcao.c
+++ funcao.c
@@ -81,6 +81,7 @@ int retorna_n_arabico(const char *get_roman)
 				return -1;
 		}
 	}
+	
 	maior_simbolo_indice = 0; // guarda o indice do maior simbolo no vetor_contador.
 	/*
 	Acha-se o índice do maior símbolo da string digitada pelo usuário.
@@ -92,9 +93,12 @@ int retorna_n_arabico(const char *get_roman)
 			maior_simbolo_indice = i;
 		}
 	}
+	/*
+	Checa a existência dessa string.
+	*/
 	check = checa_existencia_roman(get_roman, vetor_contador, vetor_frequencia, maior_simbolo_indice);
 
-	if(check == 1)
+	if(check == 1) // se este numero, de fato, existe.
 	{
 		if(maior_simbolo_indice == 0) // se o maior símbolo está no índice 0.
 		{
@@ -110,13 +114,45 @@ int retorna_n_arabico(const char *get_roman)
 			{
 				numero_em_arabico = numero_em_arabico + vetor_contador[j]; 
 			}
+
 		}
+		else // maior símbolo não está no índice 0.
+		{
+			for(k = 0;k < maior_simbolo_indice; k++) // do início da string até chegar no maior símbolo(conta a parte esquerda).
+			{
+				contador_esquerda = contador_esquerda + vetor_contador[k];		
+			}
+			for(i = maior_simbolo_indice; i < strlen(get_roman) - 1; i++)//indo de dois em dois verificando se estão na ordem adequada.
+			{
+				if(vetor_contador[i] < vetor_contador[i+1] && strlen(get_roman) > 2) // do maior símbolo até o final da string(conta a parte direita).
+					{
+						vetor_contador[i] = vetor_contador[i+1] - vetor_contador[i];
+						vetor_contador[i+1] = 0;
+						i++;
+					}
+			}
+			for(j = maior_simbolo_indice; j < strlen(get_roman); j++)
+			{
+				contador_direita = contador_direita + vetor_contador[j];
+			}
+			
+			numero_em_arabico = contador_direita - contador_esquerda;
+		}
+		free(vetor_contador);
+		free(vetor_frequencia);
 		return numero_em_arabico;
 	}
+
 	else
+	{
+		free(vetor_contador);
+		free(vetor_frequencia);
 		return -1;
+	}
+
 }
 
+/** \brief  Função que checa a validez do numero romano.*/
 int checa_existencia_roman(const char *get_roman, int *vetor_contador, int *vetor_frequencia, int maior_simbolo_indice)
 {
 
@@ -133,8 +169,7 @@ int checa_existencia_roman(const char *get_roman, int *vetor_contador, int *veto
 	/*
 	Este for checa a frequência de cada caracter, respeitado o número máximo de vezes que ele pode aparecer, tendo em mente o range
 	de 1 até 3000.
-	*/
-		
+	*/	
 	for(i = 0;i < strlen(get_roman);i = i + 2)
 	{
 		if(vetor_frequencia[i] > 3 && i!=2 && i!=4) // os caracteres I, M aparecem, no máximo, até 3 vezes
@@ -147,33 +182,37 @@ int checa_existencia_roman(const char *get_roman, int *vetor_contador, int *veto
 		}
 	}
 
+	
 	if(vetor_frequencia[2] == 4 && strlen(get_roman) == 4)// se o X apareceu 4 vezes e o tamanho da string é 4
-			return 0;
-		if(vetor_frequencia[4] == 4 && strlen(get_roman) == 4)// se o C apareceu 4 vezes e o tamanho da string é 4
-			return 0;	
-		/*
-		Caso o índice do maior símbolo estiver além do índice 1 da string, esta é, portanto, inválida.
-		*/
-		if(maior_simbolo_indice > 1)
-			return 0;
-		
-		/*
-		Este for visa a checagem da ideia de que, em qualquer caracter da string, a partir do segundo caracter anterior a ele
-		(se 'i' é o índice atual, a partir de 'i-2') só devem existir caracteres maiores ou iguais a ele.
-		Caso tenha caracteres menores, a string é inválida.
-		*/
-		for(i=2;i<strlen(get_roman);i++)
+		return 0;
+	if(vetor_frequencia[4] == 4 && strlen(get_roman) == 4)// se o C apareceu 4 vezes e o tamanho da string é 4
+		return 0;	
+	/*
+	Caso o índice do maior símbolo estiver além do índice 1 da string, esta é, portanto, inválida.
+	*/
+	if(maior_simbolo_indice > 1)
+		return 0;
+	
+	/*
+	Este for visa a checagem da ideia de que, em qualquer caracter da string, a partir do segundo caracter anterior a ele
+	(se 'i' é o índice atual, a partir de 'i-2') só devem existir caracteres maiores ou iguais a ele.
+	Caso tenha caracteres menores, a string é inválida.
+	*/
+	for(i=2;i<strlen(get_roman);i++)
+	{
+		for(j = i-2;j > 0; j--)
 		{
-			for(j = i-2;j > 0; j--)
+			if(vetor_contador[i] > vetor_contador[j])
 			{
-				if(vetor_contador[i] > vetor_contador[j])
-				{
-					return 0;
-				}
+				return 0;
 			}
 		}
+	}
 
-		if(maior_simbolo_indice > 0)
-			if(vetor_contador[maior_simbolo_indice] == vetor_contador[maior_simbolo_indice + 1])	
-				return 0;
+	if(maior_simbolo_indice > 0)
+		if(vetor_contador[maior_simbolo_indice] == vetor_contador[maior_simbolo_indice + 1])	
+			return 0;
+
+	return 1;
+
 }
